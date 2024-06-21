5b5f14f03b5f0f5d7ede8f80f5e48cd7c8fc1004
Jay Miller <kjaymiller@gmail.com>
2024-06-21 12:40:37 +0000

    moves all psql call to setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,4 +2,6 @@
 python3 -m pip install aiven-client
 
 # Installing PostgreSQL client
-apt-get -y install --no-install-recommends postgresql-client
+apt-get update && export DEBIAN_FRONTEND=noninteractive \
+&& apt-get -y install --no-install-recommends postgresql-client \
+&& apt-get clean && rm -rf /var/lib/apt/lists/*
